d596ae7cb07912b0fff2c00ad96746c1f54cb806
DinozaurG <49711433+DinozaurG@users.noreply.github.com>
2020-06-18 11:16:36 +0700

    Small update

--- Hello Color/ViewController.swift
+++ Hello Color/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isPurple = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func changeColor(_ sender: Any) {
+        if isPurple {
+            view.backgroundColor = #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1)
+            isPurple = false
+        } else {
+            view.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+            isPurple = true
+        }
+    }
+    
 }
 
